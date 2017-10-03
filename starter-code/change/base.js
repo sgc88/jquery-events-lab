b99c12379d78fff27912be3d18d6e420fa2af1f3
sgc88 <sehribancheng@yahoo.com>
2017-10-02 20:44:48 -0700

    change and click added

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -1,7 +1,19 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+	$("#time").text( Date.now() );
+  $("input.add").on("change", function handleChange(event){
 
-  $("#time").text( Date.now() );
+  	var left=$('input#left').val();
+  	var right= $('input#right').val();
+  	var leftVal = parseInt(left) || 0;
+    var rightVal = parseInt(right) || 0;
+    var totalInput= leftVal+rightVal;
+    $("#total").val(totalInput);
+    
 
-})
+
+  });
+
+});
+
--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,13 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+	$("span").on("click", function handleClick(event){
+		var call=("You clicked" +$(this).text()+ Date.now());
+		$('ul').append('<li>'+call+'</li>')
+	});
 
 })
+
+
+
+
